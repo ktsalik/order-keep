4147b54cdc3944e9a94208b72bfff9cfdbffad3e
Kostas Tsalikidis <tsalikidhs@gmail.com>
2023-01-30 06:36:48 +0200

    the right width for bottom-menu component

--- src/features/bottom-menu/BottomMenu.js
+++ src/features/bottom-menu/BottomMenu.js
@@ -1,10 +1,16 @@
+import { faCog, faFileInvoice, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './BottomMenu.scss';
 
 const BottomMenu = (props) => {
 
   return (
     <div className="BottomMenu">
-      Bottom Menu
+      <div className="content">
+        <button className="btn-menu-option"><FontAwesomeIcon icon={faUserCircle} size="3x" /></button>
+        <button className="btn-menu-option"><FontAwesomeIcon icon={faFileInvoice} size="3x" /></button>
+        <button className="btn-menu-option"><FontAwesomeIcon icon={faCog} size="3x" /></button>
+      </div>
     </div>
   );
 };
